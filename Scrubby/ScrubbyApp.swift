88de37af8a588e60d8a6a910ac08debccda52fba
Ryan Graves <get@ryangrav.es>
2025-02-16 12:44:50 -0600

    Initial Commit

---
+++ Scrubby/ScrubbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrubbyApp.swift
+//  Scrubby
+//
+//  Created by Ryan Graves on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrubbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrubbyTests/ScrubbyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrubbyTests.swift
+//  ScrubbyTests
+//
+//  Created by Ryan Graves on 2/16/25.
+//
+
+import Testing
+@testable import Scrubby
+
+struct ScrubbyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
